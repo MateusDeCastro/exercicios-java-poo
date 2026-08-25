66fa93da7019e5f748d31ca5e4e040754e647449
Mateus de Castro Farah Aranha <148548338+MateusDeCastro@users.noreply.github.com>
2026-08-24 21:42:49 -0300

    Exercicio 04

---
+++ aula-01/ex-04/Corrigir.java
@@ -0,0 +1,14 @@
+public class Corrigir{
+	public static void main(String[] args){
+		System.out.println("Olá, mundo!");
+	}
+}
+
+/*
+Corrigir.java:1: error: class Consertar is public, should be declared in a file named Consertar.java
+public class Consertar{
+       ^
+1 error
+
+Uma classe declarada como public deve conter o mesmo nome do arquivo . java. Nesse caso deve-se chamar Corrigir.
+*/
