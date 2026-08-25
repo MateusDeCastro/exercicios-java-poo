4d0d9b53dbb1cf8a42a08f96d998d8c23beaa3fe
Mateus de Castro Farah Aranha <148548338+MateusDeCastro@users.noreply.github.com>
2026-08-24 21:53:44 -0300

    Exercicio 05

---
+++ aula-01/ex-05/Arte.java
@@ -0,0 +1,16 @@
+public class Arte{
+	public static void main(String[] args){
+		System.out.println(
+		"\t\t    _\n" +
+            	"\t\t   / \\                  ___\n" +
+            	"\t\t  /   \\                |***|\n" +	
+            	"\t\t /_____\\               |***|\n" +
+            	"\t\t |  \"  |               |___|\n" +
+            	"\t\t |     |                 |\n" +
+            	"\t\t |  [] |  *  *  *  *     |\n" +
+            	"\t\t |_____| _|__|__|__|_____|\n" +
+            	"\n" +
+            	"\t\t  CASA"
+        );
+	}
+}
