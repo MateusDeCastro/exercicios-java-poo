6b3bfa1823b5689d8bb2b75497fa67abe73ade9e
Mateus de Castro Farah Aranha <148548338+MateusDeCastro@users.noreply.github.com>
2026-08-25 13:03:28 -0300

    Exercicio03

---
+++ aula-02/ex-03/Previsao.java
@@ -0,0 +1,9 @@
+public class Previsao{
+	public static void main(String[] args){
+		System.out.println(7 / 2); // 3
+		System.out.println(7 / 2.0); // 3.5
+		System.out.println(7 % 2); // 1
+		System.out.println((int) 7.9); // 7
+		System.out.println(10 / 4 * 4); // 8
+	}
+}
