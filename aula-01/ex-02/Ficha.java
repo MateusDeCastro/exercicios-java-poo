6d1408d69a701144bcdfe641bf55735ab5957531
Mateus de Castro Farah Aranha <148548338+MateusDeCastro@users.noreply.github.com>
2026-08-24 21:31:30 -0300

    Exercicio 02

---
+++ aula-01/ex-02/Ficha.java
@@ -0,0 +1,10 @@
+public class Ficha{
+	public static void main(String[] args){
+		System.out.println("Mateus De Castro");
+		System.out.println("Paulínia");
+		System.out.println("Ciência da Computação");
+		System.out.println("Desenvolver competencias na área da programação");
+		System.out.println("---------------------------------------------------------");
+		System.out.print("Mateus De Castro\nPaulínia\nCiência da Computação\nDesenvolver competencias na área da programação");
+	}
+}
