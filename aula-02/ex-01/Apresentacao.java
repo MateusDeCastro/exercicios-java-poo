d1cf4307e62087a92ffa639628cd6c4308e64eb5
Mateus de Castro Farah Aranha <148548338+MateusDeCastro@users.noreply.github.com>
2026-08-24 22:01:57 -0300

    Exercicio 01

---
+++ aula-02/ex-01/Apresentacao.java
@@ -0,0 +1,10 @@
+public class Apresentacao{
+	public static void main(String[] args){
+		String nome = "Mateus De Castro";
+		int idade = 19;
+		double altura = 1.85;
+		boolean estudar = true;
+		
+		System.out.printf("Meu nome é %s, tenho %d anos, tenho %.2f e estudo à noite: %b", nome, idade, altura, estudar);
+	}
+}
